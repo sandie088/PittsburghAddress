eae09bd7512dbd414faa809922c5959b88f9c9e2
Santhosh <sandie088@gmail.com>
2019-11-16 13:32:30 +0530

    added jdbc

--- address/src/Main.java
+++ address/src/Main.java
@@ -1,11 +1,29 @@
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
+import java.util.Properties;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        try {
+            Connection connection = createConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("Select * from address");
+            while (!resultSet.isLast())
+            {
+                resultSet.next();
+                String temp = resultSet.getString("FULL_ADDRESS");
+                System.out.println(temp);
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println("Exception in main " + e.getMessage());
+        }
     }
     
     public JSONObject getAddress() throws Exception
@@ -20,5 +38,21 @@ public class Main
         {
             throw e;
         }
+    }
+
+    private static Connection createConnection() throws IOException, ClassNotFoundException, SQLException {
+        Properties prop = new Properties();
+        String host = "jdbc:postgresql://localhost:5432/pittsburgh";
+        String username = "postgres";
+        String password = "admin";
+        Connection connection = DriverManager.getConnection(host, username, password);
+        System.out.println("CONNECTION: " + connection);
+        return connection;
+    }
+
+    private static JSONObject getSchoolForAddress(String address)
+    {
+        JSONObject addressJSON = new JSONObject();
+        return addressJSON;
     }
 }
